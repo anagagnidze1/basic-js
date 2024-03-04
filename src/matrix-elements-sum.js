b4c0fdae8e94fdaba5bf1379d093fbeaa6af19b8
Ana Gagnidze <anagagnidze32@gmail.com>
2024-03-05 00:09:50 +0400

    add

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  let blockedColumns = [];
+  
+  for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+          if (!blockedColumns.includes(j))
+          {
+              if (matrix[i][j] !== 0) {
+                  sum += matrix[i][j];
+              } else {
+                  blockedColumns.push(j);
+              }
+          }
+      }
+  }
+
+  return sum;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let resultArr = [];
+  let isPassDuplication = false;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === '--discard-next') {
+      i += 1;
+      isPassDuplication = true;
+    } else if (arr[i] === '--discard-prev') {
+      if (isPassDuplication) {
+        isPassDuplication = false;
+        continue;
+      }
+      if (resultArr.length !== 0) {
+        resultArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (i !== arr.length - 1) {
+        resultArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (isPassDuplication) {
+        isPassDuplication = false;
+        continue;
+      }
+      if (resultArr.length !== 0) {
+        resultArr.push(resultArr[resultArr.length - 1]);
+      }
+    } else {
+      resultArr.push(arr[i]);
+    }
+  }
+  
+  return resultArr;
 }
 
 module.exports = {
